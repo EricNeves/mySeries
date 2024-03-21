61d8d29fed1aed90f5d77bc4bf3bdf3ab44233d2
Eric Neves <ericnevesrodrigues@gmail.com>
2024-03-20 20:06:39 -0400

    Doc: add api doc - swagger

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,7 @@
-const express  = require('express')
-const app      = express()
-const mongoose = require('mongoose')
+const express   = require('express')
+const app       = express()
+const mongoose  = require('mongoose')
+const SwaggerUI = require('swagger-ui-express') 
 
 const PORT      = process.env.PORT || 3131
 const MONGO_URL = process.env.MONGO_URL
@@ -13,6 +14,13 @@ app.use(express.json())
 const home   = require('./routes/home')
 const user   = require('./routes/user')
 const series = require('./routes/series')
+
+/**
+ * Swagger Config
+ */
+const swagggerDoc = require('./swagger.json')
+app.use('/doc', SwaggerUI.serve, SwaggerUI.setup(swagggerDoc))
+
 
 app.use('/',       home)
 app.use('/users',  user)
